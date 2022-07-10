3129391ebfcb98c99e02bd5b17b533185630af39
Milangithub22 <95203140+Milangithub22@users.noreply.github.com>
2022-07-10 22:11:41 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  
+  return (
+    <div className="container_main">
+      <div className="game_background">
+        <h1>Tenzies</h1>
+        <p>
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
