d3f0bfbc2aa86ea091b3d135879c34e0c6bb7fc2
Milangithub22 <95203140+Milangithub22@users.noreply.github.com>
2022-07-10 22:23:58 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ function App() {
   return (
     <div className="container_main">
       <div className="game_background">
-        <h1>Tenzies</h1>
-        <p>
-          Roll until all dice are the same. Click each die to freeze it at its
-          current value between rolls.
-        </p>
+        <div className="text_container">
+          <h1>Tenzies</h1>
+          <p>
+            Roll until all dice are the same. Click each die to freeze it at its
+            current value between rolls.
+          </p>
+        </div>
       </div>
     </div>
   );
