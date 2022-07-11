28786746536d6a94a9fd63a3fabd415f28500800
Milangithub22 <95203140+Milangithub22@users.noreply.github.com>
2022-07-11 13:03:47 +0100

    adding die element with random numbers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+/* eslint-disable no-use-before-define */
+import React from "react";
 import "./App.css";
 import Die from "./Die";
+import { useState } from "react";
 
 function App() {
+  const [element, setElement] = useState(allNewDice());
+
+  function allNewDice() {
+    const newDice = [];
+
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+
+  const DieElement = element.map((item) => <Die value={item} />);
+
   return (
     <div className="container_main">
       <div className="game_background">
@@ -12,18 +28,7 @@ function App() {
             current value between rolls.
           </p>
         </div>
-        <div className="dir_container">
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-        </div>
+        <div className="dir_container">{DieElement}</div>
       </div>
     </div>
   );
