9df14a5cbbe83bbbaacb1fda8dc059f41a656a68
Milangithub22 <95203140+Milangithub22@users.noreply.github.com>
2022-07-11 17:27:36 +0100

    adding new features

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-/* eslint-disable no-use-before-define */
 import React from "react";
 import "./App.css";
 import Die from "./Die";
 import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function App() {
   const [element, setElement] = useState(allNewDice());
@@ -11,13 +11,35 @@ function App() {
     const newDice = [];
 
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6));
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: { nanoid },
+      });
     }
     return newDice;
   }
 
-  const DieElement = element.map((item) => <Die value={item} />);
+  const DieElement = element.map((item) => (
+    <Die
+      key={item.id}
+      value={item.value}
+      isHeld={item.isHeld}
+      holdDice={() => holdDice(item.id)}
+    />
+  ));
 
+  function rollDice() {
+    setElement(allNewDice());
+  }
+
+  function holdDice(id) {
+    setElement((prev) =>
+      prev.map((item) => {
+        return item.id === id ? { ...item, isHeld: !item.isHeld } : item;
+      })
+    );
+  }
   return (
     <div className="container_main">
       <div className="game_background">
@@ -28,7 +50,13 @@ function App() {
             current value between rolls.
           </p>
         </div>
-        <div className="dir_container">{DieElement}</div>
+        <div className="die_container">{DieElement}</div>
+
+        <div className="rollButton_container">
+          <button className="rollButton" onClick={rollDice}>
+            Roll
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/Die.js
+++ src/Die.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 export default function Die(props) {
+  const onClickToGreen = {
+    backgroundColor: props.isHeld ? "green" : "white",
+  };
+
   return (
-    <div className="die_face">
+    <div onClick={props.holdDice} className="die_face" style={onClickToGreen}>
       <h2 className="die_num">{props.value}</h2>
     </div>
   );
